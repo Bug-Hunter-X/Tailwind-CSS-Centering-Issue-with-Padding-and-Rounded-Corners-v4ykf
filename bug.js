a26a4af54adbb6d3e6a675a089c1c640e943f991
BugHunter-X <askzenai@gmail.com>
2025-01-18 00:42:39 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example code causing the issue
+<div class="bg-gray-200 p-4 rounded-lg">
+  <p class="text-gray-800 text-xl font-bold">This text should be centered</p>
+</div>
+```
