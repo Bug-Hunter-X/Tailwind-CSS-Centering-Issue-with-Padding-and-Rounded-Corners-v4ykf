8c7d2771ec6764a4b856998d1d23eb3937bd9586
BugHunter-X <askzenai@gmail.com>
2025-01-18 00:42:40 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// Solution
+<div class="bg-gray-200 p-4 rounded-lg flex items-center justify-center">
+  <p class="text-gray-800 text-xl font-bold">This text is now vertically and horizontally centered</p>
+</div>
+```
